b304d6a6982278bff3b2dde06681bcc67b6e0634
BlaiseAK <blaisekress@yahoo.com>
2018-06-13 21:32:37 -0400

    gifs now prepend and attach to page, next step is to get the paused versions to show then change

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,5 @@
 var gifId = "";
-var arrOfGifs = ["Captain America", "Spiderman", "Captain Marvel", "Iron Man", "Hulk", "Deadpool"];
-
-$(".super").on("click", function() {
-  var superName = $(this).attr("data-name");
-  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-  superName + "&api_key=Zz5rHijIBLCzOKNuIDNFF57DAtKMWCyI&limit=10";
-
-
-  $.ajax({
-    url: queryURL,
-    method: "GET",
-  }).then(function(response){
-    console.log(response)
-
-  })
-
-});
+var topics = ["Captain America", "Spiderman", "Captain Marvel", "Iron Man", "Hulk", "Deadpool"];
 
 function renderButtons() {
 
@@ -23,16 +7,16 @@ function renderButtons() {
     $("#superHeroButtons").empty();
 
     // Looping through the array of movies
-    for (var i = 0; i < arrOfGifs.length; i++) {
+    for (var i = 0; i < topics.length; i++) {
 
-      // Then dynamicaly generating buttons for each movie in the array.
+      // Then dynamicaly generating buttons for each super in the array.
       var a = $("<button>");
       // Adding a class
       a.addClass("supers");
-      // Adding a data-attribute with a value of the movie at index i
-      a.attr("data-name", arrOfGifs[i]);
-      // Providing the button's text with a value of the movie at index i
-      a.text(arrOfGifs[i]);
+      // Adding a data-attribute with a value of the super at index i
+      a.attr("data-name", topics[i]);
+      // Providing the button's text with a value of the super at index i
+      a.text(topics[i]);
       // Adding the button to the HTML
       $("#superHeroButtons").append(a);
     }
@@ -43,12 +27,69 @@ function renderButtons() {
 
     var hero = $("#super-input").val().trim();
 
-    arrOfGifs.push(hero);
+    topics.push(hero);
 
     renderButtons();
 
   }),
 
-  $(document).on("click", ".supers", alertSuperName);
+  $(document).on("click", ".supers", renderButtons);
 
   renderButtons();
+
+$(document).on("click", ".supers" ,function() {
+
+  var superName = $(this).data("name");
+
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+  superName + "&api_key=Zz5rHijIBLCzOKNuIDNFF57DAtKMWCyI&limit=10";
+
+
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  })
+  
+    .then(function(response){
+
+      
+    console.log(response.data)
+
+    var results = response.data;
+
+    // Looping over every result item
+    for (var i = 0; i < results.length; i++) {
+
+      // Only taking action if the photo has an appropriate rating
+      if (results[i].rating !== "r") {
+        // Creating a div with the class "item"
+        var gifDiv = $("<div class='item'>");
+
+        // Storing the result item's rating
+        var rating = results[i].rating;
+
+        // Creating a paragraph tag with the result item's rating
+        var p = $("<p>").text("Rating: " + rating);
+
+        // Creating an image tag
+        var personImage = $("<img>");
+
+        // Giving the image tag an src attribute of a proprty pulled off the
+        // result item
+        personImage.attr("src", results[i].images.fixed_height.url);
+
+        // Appending the paragraph and personImage we created to the "gifDiv" div we created
+        gifDiv.append(p);
+        gifDiv.append(personImage);
+
+        // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
+        $("#gifsAssemble").prepend(gifDiv);
+      }
+    }
+
+
+
+  })
+
+});
+
